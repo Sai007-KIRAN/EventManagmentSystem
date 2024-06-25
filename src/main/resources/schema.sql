0fce3999c3677a6d9ed6d6414479b9416222f53c
kiran <kiransaipendela@gmail.com>
2024-06-25 05:32:17 +0000

    commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE if not exists sponsor (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    industry varchar(255)
+);
+
+CREATE TABLE if not exists event (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    date varchar(255)
+);
+
+CREATE TABLE if not exists event_sponsor (
+    sponsorId INT,
+    eventId INT,
+    PRIMARY KEY (sponsorId, eventId),
+    FOREIGN KEY (sponsorId) REFERENCES sponsor(sponsorId),
+    FOREIGN KEY (eventId) REFERENCES event(eventId)
+);
